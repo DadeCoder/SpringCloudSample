c64f14e232a0aa00adf96f45622885152539bbea
DadeCoder <dade_zhang@163.com>
2017-02-18 21:20:32 +0800

    discovery services spring boot module

---
+++ discoveryservices/src/main/java/com/example/DiscoveryServicesApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServicesApplication.class, args);
+	}
+}
