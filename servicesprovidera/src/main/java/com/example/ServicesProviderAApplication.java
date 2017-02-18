ade6e446a5b84e1dac50b1b98944f65a75883bfa
DadeCoder <dade_zhang@163.com>
2017-02-18 23:45:11 +0800

    run demo from http://blog.didispace.com/springcloud1/

--- servicesprovidera/src/main/java/com/example/ServicesProviderAApplication.java
+++ servicesprovidera/src/main/java/com/example/ServicesProviderAApplication.java
@@ -2,13 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
-@EnableEurekaServer
-public class DiscoveryServicesApplication {
+public class ServicesProviderAApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryServicesApplication.class, args);
+		SpringApplication.run(ServicesProviderAApplication.class, args);
 	}
 }
