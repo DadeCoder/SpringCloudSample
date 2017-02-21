2070c7b41f372589bd31c49d04e8cafc5e21bd2b
DadeCoder <dade_zhang@163.com>
2017-02-21 22:21:20 +0800

    add hystrix for zuul

---
+++ servicegateway/src/main/java/com/example/MyFallbackProvider.java
@@ -0,0 +1,63 @@
+package com.example;
+
+import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by Dade on 2017/2/21.
+ */
+public class MyFallbackProvider implements ZuulFallbackProvider {
+    @Override
+    public String getRoute() { return "api-a"; }
+
+    @Override
+    public ClientHttpResponse fallbackResponse() {
+
+        System.out.println("--------test------------");
+        System.out.println("--------test------------");
+        System.out.println("--------test------------");
+        System.out.println("--------test------------");
+
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return 200;
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return "OK";
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("fallback".getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+    }
+}
--- servicegateway/src/main/java/com/example/ServicegatewayApplication.java
+++ servicegateway/src/main/java/com/example/ServicegatewayApplication.java
@@ -1,13 +1,16 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringCloudApplication
 @EnableZuulProxy
+//@RestController
 public class ServicegatewayApplication {
 
 	public static void main(String[] args) {
@@ -17,6 +20,19 @@ public class ServicegatewayApplication {
 	@Bean
 	public AccessFilter accessFilter() {
 		return new AccessFilter();
+	}
+
+//	@RequestMapping("/timeout")
+//	public String timeout() throws InterruptedException {
+//		Thread.sleep(80000);
+//		return "timeout";
+//	}
+
+	@Bean
+	public ZuulFallbackProvider route1ZuulFallbackProvider() {
+		GenericZuulFallbackProvider route1ZuulFallback = new GenericZuulFallbackProvider();
+		route1ZuulFallback.setRoute("COMPUTE-SERVICE");
+		return route1ZuulFallback;
 	}
 
 }
